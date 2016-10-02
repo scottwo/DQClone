ce173f2be9c1a7676747141cd5912d868d78ec84
scottwo <scott.w.ogden@gmail.com>
2016-10-02 14:03:18 -0600

    initial commit.

---
+++ Scripts/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour {
+
+	public int xSize;
+	public int zSize;
+	public PlayerManager player;
+
+	private List<GameObject> grid;
+
+	// Use this for initialization
+	void Start () {
+		GenerateGrid ();
+		AddPlayer ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void GenerateGrid() {
+		grid = new List<GameObject> ();
+		for (int i = 0; i < xSize; i++) {
+			for (int j = 0; j < zSize; j++) {
+				GameObject cube = GameObject.CreatePrimitive (PrimitiveType.Cube);
+				cube.transform.parent = gameObject.transform;
+				cube.transform.position = new Vector3 (i + 0.5f, 0.05f, j + 0.5f);
+				cube.transform.localScale = new Vector3 (1.0f, 0.1f, 1.0f);
+				grid.Add (cube);
+			}
+		}
+	}
+
+	void AddPlayer() {
+		Instantiate(
+			player, 
+			new Vector3(
+				grid[0].transform.position.x, 
+				0.1f, 
+				grid[0].transform.position.z
+			), 
+			Quaternion.identity
+		);
+	}
+}
---
+++ Scripts/PlayerManager.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerManager : MonoBehaviour {
+
+	private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CheckInput (); 	
+	}
+
+	void CheckInput() {
+		float horizontalMovement = Input.GetAxis ("Horizontal");
+		float verticalMovement = Input.GetAxis ("Vertical");
+
+		if (horizontalMovement > 0) {
+			animator.SetBool ("Run", true);
+			StartCoroutine (MoveOverSeconds (this, transform.position + Vector3.right, 0.25f));
+			transform.rotation = Quaternion.Euler (0, 90, 0);
+		}
+		if (horizontalMovement < 0) {
+			animator.SetBool ("Run", true);
+			StartCoroutine (MoveOverSeconds (this, transform.position + Vector3.left, 0.25f));
+			transform.rotation = Quaternion.Euler (0, 270, 0);
+		}
+		if (verticalMovement > 0) {
+			animator.SetBool ("Run", true);
+			StartCoroutine (MoveOverSeconds (this, transform.position + Vector3.forward, 0.25f));
+			transform.rotation = Quaternion.identity;
+		}
+		if (verticalMovement < 0) {
+			animator.SetBool ("Run", true);
+			StartCoroutine (MoveOverSeconds (this, transform.position + Vector3.back, 0.25f));
+			transform.rotation = Quaternion.Euler (0, 180, 0);
+		}
+	}
+		
+	public IEnumerator MoveOverSeconds (PlayerManager objectToMove, Vector3 end, float seconds)
+	{
+		float elapsedTime = 0;
+		Vector3 startingPos = objectToMove.transform.position;
+		while (elapsedTime < seconds)
+		{
+			objectToMove.transform.position = Vector3.Lerp(startingPos, end, (elapsedTime / seconds));
+			elapsedTime += Time.deltaTime;
+			yield return new WaitForEndOfFrame();
+		}
+		objectToMove.animator.SetBool ("Run", false);
+		objectToMove.transform.position = end;
+	}
+}
